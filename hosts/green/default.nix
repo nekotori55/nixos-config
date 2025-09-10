ad02ff40253dd9c6625221c7d37ad6c6b4ae638e
nekotori55 <nekotori55@gmail.com>
2025-09-10 22:54:23 +0300

    kinda works

--- hosts/green/default.nix
+++ hosts/green/default.nix
@@ -20,8 +20,130 @@ let
     bindl = , XF86AudioPlay, exec, playerctl play-pause
     bindl = , XF86AudioPrev, exec, playerctl previous
   '';
+
+  quic_google = builtins.fetchurl {
+    url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/quic_initial_www_google_com.bin";
+    sha256 = "02n5v8fya194ww3lffvjm2qgh1bh3m97l69q0nrnp5czfibrqn7l";
+  };
+  client_hello = builtins.fetchurl {
+    url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/tls_clienthello_www_google_com.bin";
+    sha256 = "0p172hxcfsf5knap4wdimp8vqgsbhg6cnbbb88yam07v9kp2nv4k";
+  };
+  whitelist = [
+    "dis.gd"
+    "discord-attachments-uploads-prd.storage.googleapis.com"
+    "discord.app"
+    "discord.co"
+    "discord.com"
+    "discord.design"
+    "discord.dev"
+    "discord.gift"
+    "discord.gifts"
+    "discord.gg"
+    "discord.media"
+    "discord.new"
+    "discord.store"
+    "discord.status"
+    "discord-activities.com"
+    "discordactivities.com"
+    "discordapp.com"
+    "discordapp.net"
+    "discordcdn.com"
+    "discordmerch.com"
+    "discordpartygames.com"
+    "discordsays.com"
+    "discordsez.com"
+    "stable.dl2.discordapp.net"
+    "yt3.ggpht.com"
+    "yt4.ggpht.com"
+    "yt3.googleusercontent.com"
+    "googlevideo.com"
+    "jnn-pa.googleapis.com"
+    "wide-youtube.l.google.com"
+    "youtube-nocookie.com"
+    "youtube-ui.l.google.com"
+    "youtube.com"
+    "youtubeembeddedplayer.googleapis.com"
+    "youtubekids.com"
+    "youtubei.googleapis.com"
+    "youtu.be"
+    "yt-video-upload.l.google.com"
+    "ytimg.com"
+    "ytimg.l.google.com"
+    "rutracker.org"
+  ];
+  hostlist = builtins.toFile "zapret_whitelist" (builtins.concatStringsSep "\n" whitelist);
 in
 {
+  programs.nekoray = {
+    enable = true;
+    package = pkgs.nekoray;
+    tunMode.enable = true;
+    tunMode.setuid = true;
+  };
+
+  services.zapret = {
+    enable = false;
+    package = pkgs.zapret;
+    udpSupport = true;
+    udpPorts = [
+      "50000:50099"
+      "443"
+    ];
+    httpSupport = true;
+    params = [
+      # "--socks --port=9143"
+      # "--filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=method+2,midsld,5 --dpi-desync-ttl=1 --dpi-desync-fooling=md5sig,badsum,badseq --dpi-desync-repeats=15 --dpi-desync-fake-tls=${client_hello} --hostlist ${hostlist} --new"
+      # "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=${quic_google} --hostlist ${hostlist} --new"
+      # "--filter-udp=50000-50099 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=n2 --dpi-desync-repeats=6 --hostlist ${hostlist} --new"
+
+      # Discord voice channels & screen sharing
+      "--filter-udp=50000-65535"
+      "--filter-l7=discord,stun"
+      "--dpi-desync=fake,tamper"
+      "--dpi-desync-any-protocol"
+      "--dpi-desync-cutoff=n2"
+      "--dpi-desync-repeats=6"
+      # "--hostlist ${hostlist}"
+      "--new"
+
+      "--filter-tcp=443"
+      "--dpi-desync=fake,multidisorder "
+      "--dpi-desync-split-pos=method+2,midsld,5"
+      "--dpi-desync-ttl=1"
+      "--dpi-desync-fooling=md5sig,badsum,badseq"
+      "--dpi-desync-repeats=15"
+      "--dpi-desync-fake-tls=${client_hello}"
+      "--hostlist ${hostlist}"
+      "--new"
+
+      # TCP port 80 filter (HTTP) - Youtube & Discord
+      "--filter-tcp=80"
+      "--dpi-desync=fake,split2"
+      "--dpi-desync-autottl=2"
+      "--dpi-desync-fooling=md5sig"
+      "--hostlist ${hostlist}"
+      "--new"
+
+      # TCP port 443 filter (HTTPS) - Youtube & Discord
+      "--filter-tcp=443"
+      "--dpi-desync=fake"
+      "--dpi-desync-autottl=2"
+      "--dpi-desync-fooling=badseq"
+      "--dpi-desync-fake-tls-mod=rnd,rndsni,padencap"
+      "--dpi-desync-fake-quic=${quic_google}"
+      "--hostlist ${hostlist}"
+
+      # UDP port 443 filter (QUIC) - Youtube
+      "--filter-udp=443"
+      "--dpi-desync=fake"
+      "--dpi-desync-udplen-increment=10"
+      "--dpi-desync-repeats=6"
+      "--dpi-desync-udplen-pattern=0xDEADBEEF"
+      "--hostlist ${hostlist}"
+    ];
+  };
+
   imports = [
     ./hardware.nix
   ];
@@ -52,6 +174,7 @@ in
     v2ray-geoip
     v2ray
     xray
+    zapret
     # nfqws
   ];
 
@@ -63,55 +186,57 @@ in
   #   ];
   # };
   #
-  services.zapret = {
-    enable = true;
-    params = [
-      # Discord voice channels & screen sharing
-      "--filter-udp=50000-65535"
-      "--filter-l7=discord,stun"
-      "--dpi-desync=fake,tamper"
-      "--dpi-desync-any-protocol"
-      "--new"
+  # services.zapret = {
+  #   enable = false;
+  #   params = [
+  #     # Discord voice channels & screen sharing
+  #     "--filter-udp=50000-65535"
+  #     "--filter-l7=discord,stun"
+  #     "--dpi-desync=fake,tamper"
+  #     "--dpi-desync-any-protocol"
+  #     "--new"
 
-      # TCP port 80 filter (HTTP) - Youtube & Discord
-      "--filter-tcp=80"
-      "--dpi-desync=fake,split2"
-      "--dpi-desync-autottl=2"
-      "--dpi-desync-fooling=md5sig"
-      "--new"
+  #     # TCP port 80 filter (HTTP) - Youtube & Discord
+  #     "--filter-tcp=80"
+  #     "--dpi-desync=fake,split2"
+  #     "--dpi-desync-autottl=2"
+  #     "--dpi-desync-fooling=md5sig"
+  #     "--new"
 
-      # TCP port 443 filter (HTTPS) - Youtube & Discord
-      "--filter-tcp=443"
-      # "--dpi-desync=fake"
-      "--dpi-desync=split2"
-      "--dpi-desync-autottl=2"
-      "--dpi-desync-fooling=badseq"
-      "--dpi-desync-fake-tls-mod=rnd,rndsni,padencap"
+  #     # TCP port 443 filter (HTTPS) - Youtube & Discord
+  #     "--filter-tcp=443"
+  #     "--dpi-desync=fake"
+  #     "--dpi-desync=split"
+  #     "--dpi-desync-autottl=2"
+  #     "--dpi-desync-fooling=badseq"
+  #     "--dpi-desync-fake-tls-mod=rnd,rndsni,padencap"
 
-      # UDP port 443 filter (QUIC) - Youtube
-      "--filter-udp=443"
-      "--dpi-desync=fake"
-      "--dpi-desync-udplen-increment=10"
-      "--dpi-desync-repeats=6"
-      "--dpi-desync-udplen-pattern=0xDEADBEEF"
-    ];
-    udpSupport = true;
-    udpPorts = [
-      "50000:65535"
-      "443"
-    ];
-    configureFirewall = false;
-  };
+  #     # UDP port 443 filter (QUIC) - Youtube
+  #     "--filter-udp=443"
+  #     "--dpi-desync=fake"
+  #     "--dpi-desync-udplen-increment=10"
+  #     "--dpi-desync-repeats=6"
+  #     "--dpi-desync-udplen-pattern=0xDEADBEEF"
+  #   ];
+  #   udpSupport = true;
+  #   udpPorts = [
+  #     "50000:65535"
+  #     "443"
+  #   ];
+  #   configureFirewall = true;
+  # };
+  #
+  #
 
   # systemd.services.hyprpolkitagent.wantedBy = [ "multi-user.target" ];
   #
-  services.resolved = {
-    enable = true;
-    # dnssec = "true";
-    # domains = [ "~." ];
-    # fallbackDns = [ "8.8.8.8" ];
-    # dnsovertls = "true";
-  };
+  # services.resolved = {
+  #   enable = true;
+  #   # dnssec = "true";
+  #   # domains = [ "~." ];
+  #   # fallbackDns = [ "8.8.8.8" ];
+  #   # dnsovertls = "true";
+  # };
 
   # services.zapret.enable = true;
   # services.zapret = {
@@ -149,13 +274,13 @@ in
   #   ];
   # };
 
-  programs.nekoray = {
-    enable = true;
-    tunMode = {
-      enable = true;
-      setuid = true;
-    };
-  };
+  # programs.nekoray = {
+  #   enable = true;
+  #   tunMode = {
+  #     enable = true;
+  #     setuid = true;
+  #   };
+  # };
 
   security.polkit.enable = true;
   # security.polkit.extraConfig = ''
