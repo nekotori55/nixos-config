8a10a4a2d841a6de1735c832238977d4b3fd8c55
nekotori55 <nekotori55@gmail.com>
2025-09-30 19:25:26 +0300

    podman fix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -21,6 +21,21 @@ with lib;
       uid = 1000;
       extraGroups = [ "wheel" ];
       home = "/home/${username}";
+
+      # Needed for podman shell
+      # https://gist.github.com/adisbladis/187204cb772800489ee3dac4acdd9947
+      subUidRanges = [
+        {
+          startUid = 100000;
+          count = 65536;
+        }
+      ];
+      subGidRanges = [
+        {
+          startGid = 100000;
+          count = 65536;
+        }
+      ];
     };
 
     users.users.${username} = mkAliasDefinitions options.modules.home.user;
