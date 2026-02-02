c64e6a1e979e669dcfdd66391812b765970e7231
nekotori55 <nekotori55@gmail.com>
2026-02-02 19:40:16 +0300

    system update

--- modules/android-dev.nix
+++ modules/android-dev.nix
@@ -13,9 +13,6 @@ in
   };
 
   config = mkIf config.modules.android-dev.enable {
-    # Enable ADB
-    programs.adb.enable = true;
-
     # Setup additional groups for unprivileged access
     # TODO abstract
     users.users.nekotori55.extraGroups = [
