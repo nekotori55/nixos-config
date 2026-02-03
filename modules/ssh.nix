bbdd1e898cc8518dc492c3db44490e3447290f2d
nekotori55 <nekotori55@gmail.com>
2026-02-03 19:38:19 +0300

    move public ssh-keys to separate file

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,12 +1,11 @@
 { ... }:
 let
-  hp-laptop-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYxbnJtUgpd8rBFfkg9I5HPFNZcKfU3EMGUNHoAEkrX nekotori55@green";
-  pixel-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgZMpZ0/meROz7ZsAyzVGjln/GqM5BkH93aFCxL8DEL";
+  keys = import ../other/ssh-keys.nix;
 in
 {
   users.users.nekotori55.openssh.authorizedKeys.keys = [
-    hp-laptop-key
-    pixel-key
+    keys.hp-laptop-key
+    keys.pixel-key
   ];
 
 }
---
+++ other/ssh-keys.nix
@@ -0,0 +1,4 @@
+{
+  hp-laptop-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYxbnJtUgpd8rBFfkg9I5HPFNZcKfU3EMGUNHoAEkrX nekotori55@green";
+  pixel-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgZMpZ0/meROz7ZsAyzVGjln/GqM5BkH93aFCxL8DEL";
+}
