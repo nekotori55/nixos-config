fcd3cea8a8a470b24d8631021990b95b20312c9d
nekotori55 <nekotori55@gmail.com>
2026-02-07 23:33:51 +0300

    home/git: add hide/unhide aliases

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -5,6 +5,11 @@
     settings = {
       user.email = "nekotori55@gmail.com";
       user.name = "nekotori55";
+
+      alias = {
+        hide = "update-index --assume-unchanged";
+        unhide = "update-index --no-assume-unchanged";
+      };
     };
     lfs.enable = true;
 
