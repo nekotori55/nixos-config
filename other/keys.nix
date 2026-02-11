dd8cc523c4036d6b95ea97ab4e5148cc147a1e37
nekotori55 <nekotori55@gmail.com>
2026-02-11 19:12:51 +0300

    secrets: add ember-twin key and rekey

--- other/keys.nix
+++ other/keys.nix
@@ -1,6 +1,7 @@
 {
   workstations = {
     ash-twin-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYxbnJtUgpd8rBFfkg9I5HPFNZcKfU3EMGUNHoAEkrX nekotori55@green";
+    ember-twin-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJm8eEkU4a09EZdRhhK8f19zvBzVw9k+CtL4MGwKagi";
     little-scout-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgZMpZ0/meROz7ZsAyzVGjln/GqM5BkH93aFCxL8DEL";
     interloper = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsm2SL6Fpux4w6wUVk0hchLXFB/IHrzh+IihkFg+eN2 nekotori55@nixos";
   };
