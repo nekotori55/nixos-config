65610d53f789e8b816c2236b649fc022e9db25e5
nekotori55 <nekotori55@gmail.com>
2026-03-05 01:24:42 +0300

    feat/matugen: make a color picker

--- home/desktop-environment/matugen.nix
+++ home/desktop-environment/matugen.nix
@@ -1,6 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  colorgen-pkg = pkgs.writeShellScriptBin "colorgen" ''
+    COLOR=`${pkgs.hyprpicker}/bin/hyprpicker -b`
+    MONOCHROME=`${pkgs.matugen}/bin/matugen --json hex --dry-run color hex $COLOR $wallpaper | grep -Pzo '"source_color": {\n      "dark": "#000000",\n      "default": "#000000",\n      "light": "#000000"\n    }' --count`
+    if [ $MONOCHROME == 1 ];
+    then SCHEME="-t scheme-monochrome";
+    else SCHEME="-t scheme-tonal-spot";
+    fi;
+    ${pkgs.matugen}/bin/matugen $SCHEME -r nearest --contrast 0.0 color hex $COLOR
+  '';
+in
 {
-  home.packages = with pkgs; [ matugen ];
+  home.packages = with pkgs; [
+    matugen
+    hyprpicker
+    colorgen-pkg
+  ];
 
   ricing-mode.files."matugen" = {
     source = ./dotfiles/matugen;
