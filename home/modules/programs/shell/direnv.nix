194d6ba002a991f51d5b7b66341c21830e8de00f
nekotori55 <nekotori55@gmail.com>
2026-03-09 22:49:16 +0300

    refactoring/home: organize modules

--- home/modules/programs/shell/direnv.nix
+++ home/modules/programs/shell/direnv.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ lib, config, ... }:
+let
+  inherit (lib) mkIf;
+in
 {
   programs.direnv = {
     enable = true;
     enableBashIntegration = true;
     nix-direnv.enable = true;
     silent = true;
+  };
+
+  programs.git = mkIf config.programs.direnv.enable {
+    ignores = [
+      ".direnv/"
+      ".envrc"
+    ];
   };
 }
--- home/modules/programs/shell/git.nix
+++ home/modules/programs/shell/git.nix
@@ -12,40 +12,10 @@
       };
     };
     lfs.enable = true;
-
-    ignores = [
-      ".direnv/"
-      ".envrc"
-    ];
-
   };
 
   programs.delta = {
     enable = true;
     enableGitIntegration = true;
-  };
-
-  services.git-sync = {
-    enable = true;
-
-    repositories =
-      let
-        reposPath = "/home/nekotori55";
-      in
-      {
-        wallpapers = {
-          path = "${reposPath}/wallpapers";
-          uri = "git@github.com:nekotori55/wallpapers.git";
-          interval = 60;
-          extraPackages = with pkgs; [ git-lfs ];
-        };
-
-        notes = {
-          path = "${reposPath}/notes";
-          uri = "git@github.com:nekotori55/notes.git";
-          interval = 60;
-          extraPackages = with pkgs; [ git-lfs ];
-        };
-      };
   };
 }
