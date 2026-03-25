6cb5f22ad8ccc20fa5f468eb943b5117351ee1ed
nekotori55 <nekotori55@gmail.com>
2026-03-25 14:17:59 +0300

    home/programs: install som programs

--- home/modules/programs/module.nix
+++ home/modules/programs/module.nix
@@ -6,14 +6,27 @@ let
 
   gui-programs = collectNixFiles ./gui;
   shell-programs = collectNixFiles ./shell;
+
+  discordus = (
+    pkgs.discord.override {
+      # withOpenASAR = true; # can do this here too
+      withVencord = true;
+    }
+  );
 in
 {
   imports = gui-programs ++ shell-programs;
 
   home.packages = with pkgs; [
     vesktop
+    discordus
     telegram-desktop
     obsidian
     # bitwarden-desktop
+
+    godot
+    blender
+
+    unzip
   ];
 }
