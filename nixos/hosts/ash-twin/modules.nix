ddfb4514e8bf9450336f5b5948d88646d4c48a49
nekotori55 <nekotori55@gmail.com>
2026-06-15 06:58:03 +0300

    disable/remove unused modules

--- nixos/hosts/ash-twin/modules.nix
+++ nixos/hosts/ash-twin/modules.nix
@@ -9,11 +9,8 @@ in
       enable = true;
       steam = true;
       minecraft = true;
-      gamescope = true;
-      osu = true;
     };
     antiblock.throne.enable = true;
-    virtualisation.waydroid.enable = true;
     peripherals.logitech.enable = true;
     distributed-builds.enable = true;
 
