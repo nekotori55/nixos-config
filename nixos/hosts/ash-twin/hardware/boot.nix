291ddc4844e34a35a98fd5d8d7ff6e218c13e983
nekotori55 <nekotori55@gmail.com>
2026-09-20 19:42:13 +0300

    migrate to limine with secureboot, add reboot-to-windows script

--- nixos/hosts/ash-twin/hardware/boot.nix
+++ nixos/hosts/ash-twin/hardware/boot.nix
@@ -1,14 +1,45 @@
-{ ... }:
+{ pkgs, ... }:
 {
   boot.loader = {
-    grub = {
-      enable = true;
-      device = "nodev";
-      efiSupport = true;
-      useOSProber = true;
-      default = "saved";
-      gfxmodeEfi = "1920x1080";
-    };
+    # grub = {
+    #   enable = true;
+    #   device = "nodev";
+    #   efiSupport = true;
+    #   useOSProber = true;
+    #   default = "saved";
+    #   gfxmodeEfi = "1920x1080";
+    # };
     efi.canTouchEfiVariables = true;
+  
+    limine = {
+      enable = true;
+
+      secureBoot = {
+        enable = true;
+        autoEnrollKeys.enable = false;
+      };
+
+      extraEntries = ''/Windows
+        protocol: efi
+        path: uuid(89b99b0a-0d77-4dda-9caf-1577524d5a34):/EFI/Microsoft/Boot/bootmgfw.efi
+      '';
+
+      extraConfig = ''
+        quiet: yes
+        timeout: 2
+      '';
+    };
+    
   };
+
+  environment.systemPackages =
+  let
+    reboot-to-windows = pkgs.writeShellScriptBin "reboot-to-windows" ''
+      bootctl set-default Windows && reboot
+    '';
+  in
+   [
+    pkgs.sbctl
+    reboot-to-windows
+  ];
 }
