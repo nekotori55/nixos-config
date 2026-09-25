ca2a45766b28115bbfedb06d5838b6a36383465b
nekotori55 <nekotori55@gmail.com>
2026-09-25 23:15:14 +0300

    ash-twin: enable weekly nix store clean

--- nixos/hosts/ash-twin/modules.nix
+++ nixos/hosts/ash-twin/modules.nix
@@ -7,7 +7,10 @@ in
     programs = {
       cli = {
         archive-tools.enable = true;
-        nh.enable = true;
+        nh = {
+          enable = true;
+          clean = true;
+        };
         tmux = {
           enable = true;
           # autostart = true;
--- nixos/modules/programs/cli/nh.nix
+++ nixos/modules/programs/cli/nh.nix
@@ -1,16 +1,24 @@
 { lib, config, ... }:
 let
   username = config.modules.settings.default-user.username;
+
+  cfg = config.modules.programs.cli.nh;
 in
 {
   options.modules.programs.cli.nh = {
     enable = lib.mkEnableOption "nix helper";
+    clean = lib.mkEnableOption "running nh clean weekly";
   };
 
-  config = lib.mkIf config.modules.programs.cli.nh.enable {
+  config = lib.mkIf cfg.enable {
     programs.nh = {
       enable = true;
-      flake = "/home/${username}/.config/nixos";
+      flake = lib.mkDefault "/home/${username}/.config/nixos";
+      clean = {
+        enable = cfg.clean;
+        dates = lib.mkDefault "weekly";
+        extraArgs = "--keep 5 --keep-since 3d --keep-one --optimise";
+      };
     };
   };
 }
